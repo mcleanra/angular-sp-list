4f001e84c57135bf6a5b2fbefaf80ee34b4476a9
mcleanra <mcleanra@gmail.com>
2017-04-28 15:39:06 +0200

    formatting

--- src/app/services/modelBuilder.js
+++ src/app/services/modelBuilder.js
@@ -1,84 +1,84 @@
-(function(angular){
+(function (angular) {
 
-    'use strict';
+	'use strict';
 
-angular.module('angular.sp.list')
-	.factory('modelBuilderService', [function(){
-		
-		function _buildFromRestResponse(ctor, result, spServicesJsonMapping) {
-			var json = {};
-			
-			Object.keys(result).forEach(function( key, index ){
-				
-				var mapping = spServicesJsonMapping['ows_' + key];
-				
-				if( result[key] && mapping ){
-					if( mapping.objectType == "MultiChoice" || mapping.objectType == "LookupMulti" || mapping.objectType == "UserMulti") {
-						//this is a multi lookup field, so move the values up
-						if( result[key].results ) {
-							json[mapping.mappedName] = result[key].results;
-						}
-						//remove sharepoint ;# from multiple choice fields and make it an array
-						else if( mapping.objectType == "MultiChoice" ) {
-							json[mapping.mappedName] = _.filter(result[key].split(';#'), 
-								function(item){
-									return item !== "";
-								});
+	angular.module('angular.sp.list')
+		.factory('modelBuilderService', [function () {
+
+			function _buildFromRestResponse(ctor, result, spServicesJsonMapping) {
+				var json = {};
+
+				Object.keys(result).forEach(function (key, index) {
+
+					var mapping = spServicesJsonMapping['ows_' + key];
+
+					if (result[key] && mapping) {
+						if (mapping.objectType == "MultiChoice" || mapping.objectType == "LookupMulti" || mapping.objectType == "UserMulti") {
+							//this is a multi lookup field, so move the values up
+							if (result[key].results) {
+								json[mapping.mappedName] = result[key].results;
+							}
+							//remove sharepoint ;# from multiple choice fields and make it an array
+							else if (mapping.objectType == "MultiChoice") {
+								json[mapping.mappedName] = _.filter(result[key].split(';#'),
+									function (item) {
+										return item !== "";
+									});
+							}
+							else {
+								json[mapping.mappedName] = result[key];
+							}
 						}
 						else {
 							json[mapping.mappedName] = result[key];
 						}
 					}
-					else {
-						json[mapping.mappedName] = result[key];
+				});
+
+				return new ctor(json, 'rest');
+			};
+
+			function _buildFromCamlResponse(ctor, row, spServicesJsonMapping) {
+				var json = $(row).SPXmlToJson({
+					mapping: spServicesJsonMapping,
+					includeAllAttrs: false,
+					removeOws: true
+				});
+
+				return new ctor(json, 'caml');
+			};
+
+			function _convertToSharePointListItem(item, spServicesJsonMapping) {
+				var convertedItem = angular.copy(item);
+
+				Object.keys(convertedItem).forEach(function (key, index) {
+
+					var mapping = _.findWhere(spServicesJsonMapping, { mappedName: key });
+
+					if (mapping && !mapping.objectFactory) {
+						//find the key of the mapping item, which is the name of our sharepoint column
+						var colName = _.findKey(spServicesJsonMapping, mapping);
+
+						//remove the ows_ from the front
+						colName = colName.substring(4, colName.length);
+
+						//convert this key to the actual column name from our jsonMapping
+						convertedItem[colName] = convertedItem[key];
 					}
-				}
-			});
-			
-			return new ctor(json, 'rest');
-		};
-		
-		function _buildFromCamlResponse(ctor, row, spServicesJsonMapping) {
-			var json = $(row).SPXmlToJson({
-				mapping: spServicesJsonMapping,
-				includeAllAttrs: false,
-				removeOws: true
-			});
-			
-			return new ctor(json, 'caml');
-		};
-		
-		function _convertToSharePointListItem(item, spServicesJsonMapping) {
-			var convertedItem = angular.copy(item);
-			
-			Object.keys(convertedItem).forEach(function( key, index ){
-				
-				var mapping = _.findWhere(spServicesJsonMapping, {mappedName: key});
-				
-				if( mapping && !mapping.objectFactory){
-					//find the key of the mapping item, which is the name of our sharepoint column
-					var colName = _.findKey(spServicesJsonMapping, mapping);
-					
-					//remove the ows_ from the front
-					colName = colName.substring(4, colName.length);
-					
-					//convert this key to the actual column name from our jsonMapping
-					convertedItem[colName] = convertedItem[key];
-				}
-				
-				delete convertedItem[key];
-				
-			});
-			
-			return convertedItem;
-		};
-		
-		return {
-			buildFromRestResponse: _buildFromRestResponse,
-			buildFromCamlResponse: _buildFromCamlResponse,
-			convertToSharePointListItem: _convertToSharePointListItem
-		};
-		
-	}]);
+
+					delete convertedItem[key];
+
+				});
+
+				return convertedItem;
+			};
+
+			return {
+				buildFromRestResponse: _buildFromRestResponse,
+				buildFromCamlResponse: _buildFromCamlResponse,
+				convertToSharePointListItem: _convertToSharePointListItem
+			};
+
+		}]);
 
 })(angular);
--- src/app/services/spListItem.js
+++ src/app/services/spListItem.js
@@ -1,54 +1,54 @@
 //base class for a sharepoint list item
-(function(angular){
+(function (angular) {
 
-    'use strict';
+	'use strict';
 
-angular.module('angular.sp.list')
-	.factory('spListItem', ['modelBuilderService', 'restQueryBuilderService', 
-	function(modelBuilderService, restQueryBuilderService){
-		
-		function spListItem(siteUrl, listName, viewFields, spServicesJsonMapping) {
-			this.siteUrl = siteUrl;
-			this.listName = listName;
-			this.viewFields = viewFields;
-			this.spServicesJsonMapping = spServicesJsonMapping;
-			
-			this.getListItemType = function() {
-				return "SP.Data." + this.listName.charAt(0).toUpperCase() + this.listName.split(" ").join("").slice(1) + "ListItem";
-			};
+	angular.module('angular.sp.list')
+		.factory('spListItem', ['modelBuilderService', 'restQueryBuilderService',
+			function (modelBuilderService, restQueryBuilderService) {
 
-			this.getSelectFields = function(expandAll) {
-				return restQueryBuilderService.getSelectFields(this.spServicesJsonMapping, expandAll);
-			};
-			
-			this.getExpandFields = function(expandAll) {
-				return restQueryBuilderService.getExpandFields(this.spServicesJsonMapping, expandAll);
-			};
-			
-			this.buildFromJson = function( ctor, data ) {
-				return modelBuilderService.buildFromRestResponse(ctor, data, this.spServicesJsonMapping);
-			};
+				function spListItem(siteUrl, listName, viewFields, spServicesJsonMapping) {
+					this.siteUrl = siteUrl;
+					this.listName = listName;
+					this.viewFields = viewFields;
+					this.spServicesJsonMapping = spServicesJsonMapping;
 
-			this.buildFromXml = function( ctor, data ) {
-				return modelBuilderService.buildFromCamlResponse(ctor, data, this.spServicesJsonMapping);
-			};
-			
-			this.buildListItem = function(data) {
-				return modelBuilderService.convertToSharePointListItem(data, this.spServicesJsonMapping);
-			};
-			
-			this.build = function(ctor, data) {
-				if( $.isXMLDoc(data) ) {
-					return this.buildFromXml(ctor, data);
-				}
-				else {
-					return this.buildFromJson(ctor, data);
-				}
-			};
-		};
-		
-		return spListItem;
-		
-	}]);
+					this.getListItemType = function () {
+						return "SP.Data." + this.listName.charAt(0).toUpperCase() + this.listName.split(" ").join("").slice(1) + "ListItem";
+					};
+
+					this.getSelectFields = function (expandAll) {
+						return restQueryBuilderService.getSelectFields(this.spServicesJsonMapping, expandAll);
+					};
+
+					this.getExpandFields = function (expandAll) {
+						return restQueryBuilderService.getExpandFields(this.spServicesJsonMapping, expandAll);
+					};
+
+					this.buildFromJson = function (ctor, data) {
+						return modelBuilderService.buildFromRestResponse(ctor, data, this.spServicesJsonMapping);
+					};
+
+					this.buildFromXml = function (ctor, data) {
+						return modelBuilderService.buildFromCamlResponse(ctor, data, this.spServicesJsonMapping);
+					};
+
+					this.buildListItem = function (data) {
+						return modelBuilderService.convertToSharePointListItem(data, this.spServicesJsonMapping);
+					};
+
+					this.build = function (ctor, data) {
+						if ($.isXMLDoc(data)) {
+							return this.buildFromXml(ctor, data);
+						}
+						else {
+							return this.buildFromJson(ctor, data);
+						}
+					};
+				};
+
+				return spListItem;
+
+			}]);
 
 })(angular);
