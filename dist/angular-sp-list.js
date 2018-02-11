913da92524f0f45b80e478dfc30e809eaa1c6bd8
mcleanra <mcleanra@gmail.com>
2018-02-11 16:01:25 +0100

    included latest build

--- dist/angular-sp-list.js
+++ dist/angular-sp-list.js
@@ -175,6 +175,128 @@
 		}]);
 
 })(angular);
+(function (angular) {
+
+	'use strict';
+
+	angular.module('angular.sp.list')
+		.factory('spBuiltInFieldNames', [function () {
+				return {
+					ows_ID: { mappedName: 'Id', objectType: "Number" },
+					ows_ContentTypeId: { mappedName: 'ContentTypeId', objectType: 'ContentTypeId' },
+					ows_ContentType: { mappedName: 'ContentType', objectType: 'Text' },
+					ows_Created: { mappedName: 'Created', objectType: 'DateTime' },
+					ows_Author: { mappedName: 'Author', objectType: 'User' },
+					ows_Modified: { mappedName: 'Modified', objectType: 'DateTime' },
+					ows_Editor: { mappedName: 'Editor', objectType: 'User' },
+					ows__HasCopyDestinations: { mappedName: '_HasCopyDestinations', objectType: 'Boolean' },
+					ows__CopySource: { mappedName: '_CopySource', objectType: 'Text' },
+					ows__ModerationStatus: { mappedName: '_ModerationStatus', objectType: 'ModStat' },
+					ows__ModerationComments: { mappedName: '_ModerationComments', objectType: 'Note' },
+					ows_FileRef: { mappedName: 'FileRef', objectType: 'Lookup' },
+					ows_FileDirRef: { mappedName: 'FileDirRef', objectType: 'Lookup' },
+					ows_Last_x0020_Modified: { mappedName: 'Last_x0020_Modified', objectType: 'Lookup' },
+					ows_Created_x0020_Date: { mappedName: 'Created_x0020_Date', objectType: 'Lookup' },
+					ows_File_x0020_Size: { mappedName: 'File_x0020_Size', objectType: 'Lookup' },
+					ows_FSObjType: { mappedName: 'FSObjType', objectType: 'Lookup' },
+					ows_PermMask: { mappedName: 'PermMask', objectType: 'Computed' },
+					ows_CheckedOutUserId: { mappedName: 'CheckedOutUserId', objectType: 'Lookup' },
+					ows_IsCheckedoutToLocal: { mappedName: 'IsCheckedoutToLocal', objectType: 'Lookup' },
+					ows_CheckoutUser: { mappedName: 'CheckoutUser', objectType: 'User' },
+					ows_FileLeafRef: { mappedName: 'FileLeafRef', objectType: 'File' },
+					ows_UniqueId: { mappedName: 'UniqueId', objectType: 'Lookup' },
+					ows_ProgId: { mappedName: 'ProgId', objectType: 'Lookup' },
+					ows_ScopeId: { mappedName: 'ScopeId', objectType: 'Lookup' },
+					ows_VirusStatus: { mappedName: 'VirusStatus', objectType: 'Lookup' },
+					ows_CheckedOutTitle: { mappedName: 'CheckedOutTitle', objectType: 'Lookup' },
+					ows__CheckinComment: { mappedName: '_CheckinComment', objectType: 'Lookup' },
+					ows_LinkCheckedOutTitle: { mappedName: 'LinkCheckedOutTitle', objectType: 'Computed' },
+					ows_Modified_x0020_By: { mappedName: 'Modified_x0020_By', objectType: 'Text' },
+					ows_Created_x0020_By: { mappedName: 'Created_x0020_By', objectType: 'Text' },
+					ows_File_x0020_Type: { mappedName: 'File_x0020_Type', objectType: 'Text' },
+					ows_HTML_x0020_File_x0020_Type: { mappedName: 'HTML_x0020_File_x0020_Type', objectType: 'Text' },
+					ows__SourceUrl: { mappedName: '_SourceUrl', objectType: 'Text' },
+					ows__SharedFileIndex: { mappedName: '_SharedFileIndex', objectType: 'Text' },
+					ows__EditMenuTableStart: { mappedName: '_EditMenuTableStart', objectType: 'Computed' },
+					ows__EditMenuTableEnd: { mappedName: '_EditMenuTableEnd', objectType: 'Computed' },
+					ows_LinkFilenameNoMenu: { mappedName: 'LinkFilenameNoMenu', objectType: 'Computed' },
+					ows_LinkFilename: { mappedName: 'LinkFilename', objectType: 'Computed' },
+					ows_DocIcon: { mappedName: 'DocIcon', objectType: 'Computed' },
+					ows_ServerUrl: { mappedName: 'ServerUrl', objectType: 'Computed' },
+					ows_EncodedAbsUrl: { mappedName: 'EncodedAbsUrl', objectType: 'Computed' },
+					ows_BaseName: { mappedName: 'BaseName', objectType: 'Computed' },
+					ows_FileSizeDisplay: { mappedName: 'FileSizeDisplay', objectType: 'Computed' },
+					ows_MetaInfo: { mappedName: 'MetaInfo', objectType: 'Lookup' },
+					ows__Level: { mappedName: '_Level', objectType: 'Integer' },
+					ows__IsCurrentVersion: { mappedName: '_IsCurrentVersion', objectType: 'Boolean' },
+					ows_SelectTitle: { mappedName: 'SelectTitle', objectType: 'Computed' },
+					ows_SelectFilename: { mappedName: 'SelectFilename', objectType: 'Computed' },
+					ows_Edit: { mappedName: 'Edit', objectType: 'Computed' },
+					ows_owshiddenversion: { mappedName: 'owshiddenversion', objectType: 'Integer' },
+					ows__UIVersion: { mappedName: '_UIVersion', objectType: 'Integer' },
+					ows__UIVersionString: { mappedName: '_UIVersionString', objectType: 'Text' },
+					ows_InstanceID: { mappedName: 'InstanceID', objectType: 'Integer' },
+					ows_Order: { mappedName: 'Order', objectType: 'Number' },
+					ows_GUID: { mappedName: 'GUID', objectType: 'Guid' },
+					ows_WorkflowVersion: { mappedName: 'WorkflowVersion', objectType: 'Integer' },
+					ows_WorkflowInstanceID: { mappedName: 'WorkflowInstanceID', objectType: 'Guid' },
+					ows_ParentVersionString: { mappedName: 'ParentVersionString', objectType: 'Lookup' },
+					ows_ParentLeafName: { mappedName: 'ParentLeafName', objectType: 'Lookup' },
+					ows_Title: { mappedName: 'Title', objectType: 'Text' },
+					ows_TemplateUrl: { mappedName: 'TemplateUrl', objectType: 'Text' },
+					ows_xd_ProgID: { mappedName: 'xd_ProgID', objectType: 'Text' },
+					ows_xd_Signature: { mappedName: 'xd_Signature', objectType: 'Boolean' },
+					ows_Combine: { mappedName: 'Combine', objectType: 'Computed' },
+					ows_RepairDocument: { mappedName: 'RepairDocument', objectType: 'Computed' }
+				};
+			}]);
+
+})(angular);
+(function (angular) {
+
+	'use strict';
+
+	angular.module('angular.sp.list')
+		.factory('spFieldTypes', [function () {
+				return {
+					'Invalid': 0,
+					'Integer': 1,
+					'Text': 2,
+					'Note': 3,
+					'DateTime': 4,
+					'Counter': 5,
+					'Choice': 6,
+					'Lookup': 7,
+					'Boolean': 8,
+					'Number': 9,
+					'Currency': 10,
+					'URL': 11,
+					'Computed': 12,
+					'Threading': 13,
+					'Guid': 14,
+					'MultiChoice': 15,
+					'GridChoice': 16,
+					'Calculated': 17,
+					'File': 18,
+					'Attachments': 19,
+					'User': 20,
+					'Recurrence': 21,
+					'CrossProjectLink': 22,
+					'ModStat': 23,
+					'Error': 24,
+					'ContentTypeID': 25,
+					'PageSeparator': 26,
+					'ThreadIndex': 27,
+					'WorkflowStatus': 28,
+					'AllDayEvent': 29,
+					'WorkflowEventType': 30,
+					'Geolocation': null, //not in the documentation
+					'OutcomeChoice': null, //not in the documentation
+					'MaxItems': 31
+				}
+			}]);
+
+})(angular);
 //base class for a sharepoint list item
 (function (angular) {
 
@@ -236,8 +358,8 @@
 	'use strict';
 
 	angular.module('angular.sp.list')
-		.factory('spListService', ['$http', '$q', '_', 'RequestDigestIntervalService', 'RequestDigestCacheService',
-			function ($http, $q, _, RequestDigestIntervalService, RequestDigestCacheService) {
+		.factory('spListService', ['$http', '$q', '_', 'RequestDigestIntervalService', 'RequestDigestCacheService', 'spFieldTypes', 'spBuiltInFieldNames',
+			function ($http, $q, _, RequestDigestIntervalService, RequestDigestCacheService, spFieldTypes, spBuiltInFieldNames) {
 
 				function spListService(spListItem) {
 
@@ -246,20 +368,24 @@
 					//start maintaining a request digest for this site in case they have to post
 					RequestDigestIntervalService.start(spListItem.prototype.siteUrl);
 
+					//convenience function to select list items from this list using a $filter
 					this.getByFilter = function (filter, select) {
 						return this.executeRestQuery(null, select, filter, null);
 					};
 
 					this.getByFilters = this.getByFilter;
 
+					//gets all list items from this list using the default view
 					this.get = function () {
 						return this.executeRestQuery(null, null, null, null);
 					};
 
+					//gets a list item by its id
 					this.getById = function (id) {
 						return this.getByArrayOfIds([id]);
 					};
 
+					//get list items from this list by an array of list item ids
 					this.getByArrayOfIds = function (idArray) {
 						var valueArray = [];
 
@@ -271,6 +397,78 @@
 						return this.executeCamlQuery(query);
 					};
 
+					//creates this list with the columns from the model
+					this.provisionList = function() {
+
+						var requestHeaders = {
+							"accept": "application/json;odata=verbose",
+							"X-RequestDigest": RequestDigestCacheService.get(spListItem.prototype.siteUrl),
+							"content-type": "application/json;odata=verbose",
+							"If-Match": "*",
+							"X-HTTP-Method": "POST"
+						};
+						var data = {
+							__metadata: { "type": "SP.List" },
+							"AllowContentTypes": true,
+							"BaseTemplate": 100,
+							"ContentTypesEnabled": true,
+							"Description": "",
+							"Title":  spListItem.prototype.listName
+						};
+						data = angular.extend({}, item, data);
+
+						var requestURI = spListItem.prototype.siteUrl + "/_api/web/lists";
+						var requestBody = JSON.stringify(data);
+
+						return $http({
+							method: 'POST',
+							url: requestURI,
+							contentType: "application/json;odata=verbose",
+							data: requestBody,
+							headers: requestHeaders
+						}).then(function (response) {
+							_.each(spListItem.prototype.spServicesJsonMapping, function(mapping, key){
+								if( spBuiltInFieldNames[key] ) {
+									//if this is a built-in field name, don't try to provision it
+								}
+								else {
+									svc.provisionField(mapping.mappedName, spFieldTypes[mapping.objectType]);
+								}
+							});
+						});
+					};
+
+					//adds a column to this sharepoint list
+					this.provisionField = function(title, type) {
+						var requestHeaders = {
+							"accept": "application/json;odata=verbose",
+							"X-RequestDigest": RequestDigestCacheService.get(spListItem.prototype.siteUrl),
+							"content-type": "application/json;odata=verbose",
+							"If-Match": "*",
+							"X-HTTP-Method": "POST"
+						};
+						var data = {
+							__metadata: { "type": "SP.Field" },
+							"Title": title,
+							"FieldTypeKind": type
+						};
+						data = angular.extend({}, item, data);
+
+						var requestURI = spListItem.prototype.siteUrl + "/_api/web/lists/GetByTitle('" + spListItem.prototype.listName + "')/Fields";
+						var requestBody = JSON.stringify(data);
+
+						return $http({
+							method: 'POST',
+							url: requestURI,
+							contentType: "application/json;odata=verbose",
+							data: requestBody,
+							headers: requestHeaders
+						}).then(function (response) {
+							return response;
+						});
+					};
+
+					//creates an item in this list
 					this.create = function (item) {
 
 						item = new spListItem(item);
@@ -301,7 +499,8 @@
 							return response.data.d;
 						});
 					};
-
+					
+					//updates an existing item in this list
 					this.update = function (item) {
 
 						item = new spListItem(item);
@@ -331,6 +530,7 @@
 						});
 					};
 
+					//deletes an item from this list
 					this.remove = function (item) {
 
 						item = new spListItem(item);
@@ -360,6 +560,7 @@
 						});
 					};
 
+					//executes a caml query on this list
 					this.executeCamlQuery = function (query) {
 
 						var requestURI = spListItem.prototype.siteUrl + "/_vti_bin/Lists.asmx";
@@ -399,6 +600,7 @@
 
 					};
 
+					//executes a rest query to this list
 					this.executeRestQuery = function (top, select, filter, expand) {
 						var requestURI = spListItem.prototype.siteUrl + "/_api/web/lists/GetByTitle('" + spListItem.prototype.listName + "')/Items";
 
